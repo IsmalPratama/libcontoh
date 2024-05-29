c10f8c40438794129321447422049a5c1c87f7b1
IsmalPratama <ismalpratama5@gmail.com>
2024-05-29 09:31:04 +0700

    commit pertama

---
+++ src/helloworld.php
@@ -0,0 +1,11 @@
+<?php
+namespace ismal\libhallo;
+
+class helloworld{
+    public function sayHello ($name){
+        return "Hello $name welcome to php and composer to create your on library";
+    }
+}
+
+
+?>
